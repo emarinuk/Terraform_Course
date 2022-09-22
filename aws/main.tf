ba5227169e7837d734a5c57ee161b83fe9c36298
Eduardo Marin <mr.eduardo.marin@gmail.com>
2022-09-22 22:02:49 +0100

    Lesson 20

--- aws/main.tf
+++ aws/main.tf
@@ -1,15 +1,21 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.31.0"
     }
   }
 }
 
 provider "aws" {
-  region = "eu-west-2"# Configuration options
+  region     = "eu-west-2" # Configuration options
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "My VPC"
+  }
+}
--- gcp/main.tf
+++ gcp/main.tf
@@ -1,18 +1,19 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.37.0"
     }
   }
 }
 
-
 provider "google" {
-  credentials = file("terraform-course-363310-ed53e73e81e1.json")
-
-  project = "terraform-course-363310"
-  region  = "europe-southwest1"
-  zone    = "europe-southwest1-b"
+  credentials = file("terraform-course-363310-b0e98b288c2b.json")
+  project     = "terraform-course-363310"
+  region      = "europe-southwest1"
+  zone        = "europe-southwest1-b"
 }
 
+resource "google_compute_network" "my-vpc" {
+  name = "my-vpc"
+}
