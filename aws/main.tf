c1b89697a28cd136f20ec578ea55411a8f7b74a4
Eduardo Marin <mr.eduardo.marin@gmail.com>
2022-09-23 13:49:32 +0100

    Lesson 35

--- aws/main.tf
+++ aws/main.tf
@@ -46,6 +46,19 @@ resource "aws_default_route_table" "vpc_default_rt" {
   }
 }
 
+data "aws_ami" "latest_amazon_linux2" {
+  owners = ["amazon"]
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+}
+
 resource "aws_default_security_group" "default_security_group" {
   vpc_id = aws_vpc.my_vpc.id
   ingress {
@@ -73,7 +86,7 @@ resource "aws_default_security_group" "default_security_group" {
 }
 
 resource "aws_instance" "my_vm" {
-  ami = "ami-06672d07f62285d1d"
+  ami = data.aws_ami.latest_amazon_linux2.id
   instance_type = "t2.micro"
   subnet_id = aws_subnet.my_subnet.id
   vpc_security_group_ids = [aws_default_security_group.default_security_group.id]
