a95b4d881296eea4965159ca131d621f2f47ea66
Eduardo Marin <mr.eduardo.marin@gmail.com>
2022-09-23 11:14:25 +0100

    Lesson 29

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,18 +1,18 @@
 variable "vpc_cidr_block" {
-  # default = "10.0.0.0/16"
+  default = "10.0.0.0/16"
   description = "CIDR Block for the VPC"
   type = string
 }
 
 variable "web_subnet" {
-  # default = "10.0.10.0/24"
+  default = "10.0.10.0/24"
   description = "Web Subnet"
   type = string
 }
 
 variable "subnet_zone" {
-  default = "10.0.0.0/16"
-  description = "CIDR Block for the subnet"
+  default = "eu-west-2b"
+  description = "AZ for subnet"
   type = string
 }
 
